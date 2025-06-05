41d15a2da9077effa11019b7228b47a5aba95420
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-06-05 12:55:34 -0700

    fix: dirname replacement

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
 
 
 export default (script: string) => {
-    let dir = path.resolve(import.meta.url, '../'),
+    let dir = path.resolve(fileURLToPath(import.meta.url), '../'),
         i = 0,
         root = path.parse(dir).root;
 
-    while (dir !== root && i++ < 10) {
+    while (dir !== root && i++ < 100) {
         let filepath = path.resolve(dir, `node_modules/.bin/${script}`);
 
         if (fs.existsSync(filepath)) {
