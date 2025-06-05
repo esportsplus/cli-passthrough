740d29d0a64fcc775f23fb9118d408184e796bcb
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-06-05 12:37:45 -0700

    fix: script

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { spawn } from 'node:child_process';
-import fs from 'node:fs';
-import path from 'node:path';
+import { spawn } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
 
 export default (script: string) => {
     let dir = path.resolve(import.meta.url, '../'),
+        i = 0,
         root = path.parse(dir).root;
 
-    while (dir !== root) {
+    while (dir !== root && i++ < 10) {
         let filepath = path.resolve(dir, `node_modules/.bin/${script}`);
 
         if (fs.existsSync(filepath)) {
